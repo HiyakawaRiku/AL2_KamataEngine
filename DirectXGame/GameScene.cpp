61636174d3f44303942074bd093d1cf0fbac8c2f
HIyakawaRiku <k025g0059@g.neec.ac.jp>
2025-12-27 09:31:54 +0900

    modelBlock_でエラーが起きた。

--- DirectXGame/GameScene.cpp
+++ DirectXGame/GameScene.cpp
@@ -2,8 +2,41 @@
 
 using namespace KamataEngine;
 
-void GameScene::Initialize() {}
+void GameScene::Initialize() {
+	model_ = Model::Create();
 
-void GameScene::Update() {}
+	const uint32_t kNumBlockHorizontal = 20;
+	const float kBlockWidth = 2.0f;
+	worldTransformBlocks_.resize(kNumBlockHorizontal);
 
-void GameScene::Draw() {}
+	for (uint32_t i = 0; i < kNumBlockHorizontal; i++) {
+		worldTransformBlocks_[i] = new WorldTransform();
+		worldTransformBlocks_[i]->Initialize();
+		worldTransformBlocks_[i]->translation_.x = kBlockWidth * i;
+		worldTransformBlocks_[i]->translation_.y = 0.0f;
+	}
+}
+
+void GameScene::Update() {
+	for (WorldTransform* worldTransformBlock : worldTransformBlocks_) {
+
+		//アフィン変換行列の作成
+		
+		worldTransformBlock->matWorld_ = // アフィン変換行列;
+		    worldTransformBlock->TransferMatrix();
+	}
+}
+
+void GameScene::Draw() {
+	for (WorldTransform* worldTransformBlock : worldTransformBlocks_) {
+		modelBlock_->Draw(*worldTransformBlock,camera_)
+	}
+}
+
+GameScene::~GameScene() { 
+	delete model_; 
+	for (WorldTransform* worldTransformBlock : worldTransformBlocks_) {
+		delete worldTransformBlock;
+	}
+	worldTransformBlocks_.clear();
+	}
