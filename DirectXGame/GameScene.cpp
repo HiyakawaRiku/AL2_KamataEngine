7a183080d87b52c2b0edfdfc082b7145b1fe8f81
HIyakawaRiku <k025g0059@g.neec.ac.jp>
2026-01-05 08:04:00 +0900

    マップチップ座標をゲームシーンに持たせるところまで

--- DirectXGame/GameScene.cpp
+++ DirectXGame/GameScene.cpp
@@ -2,8 +2,29 @@
 
 using namespace KamataEngine;
 
-void GameScene::Initialize() {}
+void GameScene::Initialize() {
+	mapChipField_ = new MapChipField;
+	mapChipField_->LoadMapChipCsv("Resources/Block.csv");
+	GenerateBlocks();
+}
 
 void GameScene::Update() {}
 
 void GameScene::Draw() {}
+
+GameScene::~GameScene() { delete mapChipField_; }
+
+void GameScene::GenerateBlocks() { 
+	//要素数
+	uint32_t numBlockVirtical = mapChipField_->GetNumBlockVirtical();
+	uint32_t numBlockHorizontal = mapChipField_->GetNumBlockHorizontal();
+
+	//要素数を変更する
+	//列数を設定(縦方向のブロック数)
+	worldTransformBlocks_.resize();
+	for (uint32_t i = 0; i < ; ++i) {
+	
+	//1列の要素数を設定(横方向のブロック数)
+		worldTransformBlocks_[i].resize();
+	}
+}
--- DirectXGame/GameScene.h
+++ DirectXGame/GameScene.h
@@ -1,12 +1,18 @@
 #pragma once
 #include "KamataEngine.h"
+#include "MapChipField.h"
 #include <vector>
 
 class GameScene {
 public:
 	void Initialize();
-
 	void Update();
-
 	void Draw();
+	~GameScene();
+
+	void GenerateBlocks();
+
+private:
+	//マップチップフィールド
+	MapChipField* mapChipField_;
 };
